a4db12e99fe7e4b2ffa3718dfbbf6f92f78064f6
hyerim108 <batlove108@naver.com>
2023-04-01 18:46:17 +0900

    Module ex06

---
+++ Module01/ex06/Harl.cpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyerimki <hyerimki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/01 17:11:04 by hyerimki          #+#    #+#             */
+/*   Updated: 2023/04/01 18:05:18 by hyerimki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void Harl::complain(std::string level)
+{
+    void (Harl::*complan_ptr[4])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    
+    for (int i=0;i<4;i++)
+    {
+        if (levels[i] == level)
+        {
+            (this->*complan_ptr[i])();
+            break;
+        }
+    }
+}
+
+void Harl::debug(void)
+{
+    std::cout << "[DEBUG]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+    std::cout << std::endl;
+}
+
+void Harl::info(void)
+{
+    std::cout << "[INFO]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "[WARNING]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout << "[ERROR]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << std::endl;
+}
+
+int arg_transper(char *str)
+{
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    for(int i=0;i<4;i++)
+    {
+        if (levels[i] == str)
+            return (i);
+    }
+    return (-1);
+}
+
+void arg_check(char *str)
+{
+    Harl harl;
+    switch(arg_transper(str))
+    {
+        case 0:
+            harl.complain("DEBUG");
+        case 1:
+            harl.complain("INFO");
+        case 2:
+            harl.complain("WARNING");
+        case 3:
+            harl.complain("ERROR");
+            break;
+        default:
+            std::cout << "[filter : DEBUG , INFO , WARNING, ERROR]"  << std::endl;
+    }
+}
---
+++ Module01/ex06/Harl.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyerimki <hyerimki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/01 17:11:39 by hyerimki          #+#    #+#             */
+/*   Updated: 2023/04/01 17:52:07 by hyerimki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl{
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+    public:
+        void complain(std::string level);
+};
+
+void arg_check(char *str);
+
+#endif
