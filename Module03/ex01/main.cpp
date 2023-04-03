e96b843b336402b81cc1581cce0a4057c9d3d961
hyerim108 <batlove108@naver.com>
2023-04-03 20:07:20 +0900

    Module03 ex01

---
+++ Module03/ex01/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyerimki <hyerimki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/03 16:42:00 by hyerimki          #+#    #+#             */
+/*   Updated: 2023/04/03 18:19:56 by hyerimki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+    ClapTrap a = ClapTrap("A");
+	ClapTrap b = ClapTrap("B");
+
+	a.attack("B");
+	b.takeDamage(5);
+	b.beRepaired(3);
+	b.attack("A");
+	a.takeDamage(10);
+	a.beRepaired(10);
+	return (0);
+}
