ff5831b0f683d7d8c9e3cb59c714de4e955b4443
hyerim108 <batlove108@naver.com>
2023-04-06 13:38:14 +0900

    Module ex01

--- Module03/ex01/ScavTrap.cpp
+++ Module03/ex01/ScavTrap.cpp
@@ -6,7 +6,31 @@
 /*   By: hyerimki <hyerimki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 20:06:58 by hyerimki          #+#    #+#             */
-/*   Updated: 2023/04/03 20:06:59 by hyerimki         ###   ########.fr       */
+/*   Updated: 2023/04/03 20:22:39 by hyerimki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap(void) : ClapTrap::ClapTrap()
+{
+    std::cout << "ScavTrap was constructed" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name) : ClapTrap::ClapTrap(name)
+{
+    hit = 100;
+    energy = 50;
+    attackDamage = 20;
+    std::cout << "ScavTrap <" << name << "> was constructed" << std::endl;
+}
+
+ScavTrap::ScavTrap(const ScavTrap &scavtrap)
+{
+    *this = scavtrap;
+}
+
+ScavTrap &ScavTrap::operator=(const ScavTrap &scavtrap)
+{
+    
+}
--- Module03/ex01/ScavTrap.hpp
+++ Module03/ex01/ScavTrap.hpp
@@ -6,7 +6,25 @@
 /*   By: hyerimki <hyerimki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 20:07:01 by hyerimki          #+#    #+#             */
-/*   Updated: 2023/04/03 20:07:01 by hyerimki         ###   ########.fr       */
+/*   Updated: 2023/04/03 20:20:07 by hyerimki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+#include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap{
+    public:
+        ScavTrap(void);
+        ScavTrap(std::string name);
+        ScavTrap(const ScavTrap &scavtrap);
+        ScavTrap &operator=(const ScavTrap &scavtrap);
+        ~ScavTrap(void);
+        
+};
+#endif
