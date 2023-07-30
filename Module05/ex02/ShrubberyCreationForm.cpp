6e9093fa35a8f5b388eaf67c853df95672fff36c
hyerim108 <batlove108@naver.com>
2023-07-30 14:52:57 +0900

    CPP Module ex02

--- Module05/ex02/ShrubberyCreationForm.cpp
+++ Module05/ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: hyerimki <hyerimki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:07:36 by hyerimki          #+#    #+#             */
-/*   Updated: 2023/07/26 15:38:28 by hyerimki         ###   ########.fr       */
+/*   Updated: 2023/07/30 14:52:04 by hyerimki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,8 @@ void ShrubberyCreationForm::execute(const Bureaucrat &excutor) const
 {
     AForm::checkExce(excutor);
     //ascii
-    std::ofstream FileName(this->target + "_shrubbery");
+    std::ofstream FileName;
+    FileName.open((this->target + "_shrubbery").c_str(), std::ios::out);
 	FileName << "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n\
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠓⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠀⠀⠀⠀⠀⢠⢤⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n\
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠔⠒⠒⠲⠎⠀⠀⢹⡃⢀⣀⠀⠑⠃⠀⠈⢀⠔⠒⢢⠀⠀⠀⡖⠉⠉⠉⠒⢤⡀⠀⠀⠀⠀⠀\n\
